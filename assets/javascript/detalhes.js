1a946946bc4c49bd09608f1947e854ead0726181
Bwraian <wuigor12@gmail.com>
2024-06-28 19:36:05 -0300

    ATUALIZAÇÃO

--- assets/javascript/detalhes.js
+++ assets/javascript/detalhes.js
@@ -1,41 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Função para obter o valor de um parâmetro da URL
-  function getParameterByName(name) {
-    const url = new URL(window.location.href);
-    return url.searchParams.get(name);
-  }
+  const urlParams = new URLSearchParams(window.location.search);
+  const gameId = urlParams.get('gameId');
 
-  // Obtenha o ID do jogo a partir da URL
-  const gameId = getParameterByName("gameId");
-
-  // Função para carregar e exibir os dados do jogo
-  function loadGame(gameId, games) {
-    const game = games[gameId];
-    if (game) {
-      document.getElementById("game-title").innerText = game.title;
-      document.getElementById("game-image").src = game.image;
-      document.getElementById("video-source").src = game.video;
-      document.getElementById("game-video").load(); // Recarrega o vídeo
-      document.getElementById("game-description").innerText = game.description;
-      document.getElementById("game-rating").innerText = game.rating;
-      document.getElementById("game-genre").innerText = game.genre;
-      console.log(game);
-    } else {
-      console.error("Jogo não encontrado: " + gameId);
-    }
-  }
-
-  // Carregue o JSON e exiba as informações do jogo
   fetch("infojogos.json")
-    .then((response) => response.json())
-    .then((games) => {
-      if (gameId) {
-        loadGame(gameId, games);
+    .then(response => response.json())
+    .then(games => {
+      if (gameId && games[gameId]) {
+        let game = games[gameId];
+        document.getElementById("game-title").innerText = game.title;
+        document.getElementById("game-image").src = game.image;
+        document.getElementById("video-source").src = game.video;
+        document.getElementById("game-video").load();
+        document.getElementById("game-description").innerText = game.description;
+        document.getElementById("game-rating").innerText = game.rating;
+        document.getElementById("game-genre").innerText = game.genre;
+
+       
+        let gameLink = document.getElementById("game-link");
+        gameLink.innerText = "Link Dos Desenvolvedores";
+        gameLink.href = game.link;
+        gameLink.target = "_blank"; 
       } else {
-        console.error("Nenhum ID de jogo especificado na URL.");
+        console.error("Jogo não encontrado: " + gameId);
       }
     })
-    .catch((error) =>
-      console.error("Erro ao carregar os dados dos jogos: ", error)
-    );
+    .catch(error => console.error("Erro ao carregar os dados dos jogos: ", error));
 });
